cf7ff6cefeff28c3504f305cbbc7b5ebad5dab2a
lisitsinyr <lisitsinyr@gmail.com>
2024-04-23 09:47:38 +0400

    20240423

--- PY/LUos.py
+++ PY/LUos.py
@@ -74,6 +74,40 @@ if platform.system() == 'Windows':
 
 import LUDateTime
 
+#Correspondence to tools in the os module
+#-----------------------------------------------------------------------------
+# os and os.path                      pathlib
+# -----------------------------------------------------------------------------
+# os.path.abspath()                   Path.absolute() [1]
+# os.path.realpath()                  Path.resolve()
+# os.chmod()                          Path.chmod()
+# os.mkdir()                          Path.mkdir()
+# os.makedirs()                       Path.mkdir()
+# os.rename()                         Path.rename()
+# os.replace()                        Path.replace()
+# os.rmdir()                          Path.rmdir()
+# os.remove(), os.unlink()            Path.unlink()
+# os.getcwd()                         Path.cwd()
+# os.path.exists()                    Path.exists()
+# os.path.expanduser()                Path.expanduser() and Path.home()
+# os.listdir()                        Path.iterdir()
+# os.walk()                           Path.walk()
+# os.path.isdir()                     Path.is_dir()
+# os.path.isfile()                    Path.is_file()
+# os.path.islink()                    Path.is_symlink()
+# os.link()                           Path.hardlink_to()
+# os.symlink()                        Path.symlink_to()
+# os.readlink()                       Path.readlink()
+# os.path.relpath()                   PurePath.relative_to() [2]
+# os.stat()                           Path.stat(), Path.owner(), Path.group()
+# os.path.isabs()                     PurePath.is_absolute()
+# os.path.join()                      PurePath.joinpath()
+# os.path.basename()                  PurePath.name
+# os.path.dirname()                   PurePath.parent
+# os.path.samefile()                  Path.samefile()
+# os.path.splitext()                  PurePath.stem and PurePath.suffix
+
+
 cHOME = 'HOME'
 cWINDIR = 'windir'
 cTEST = 'TEST'
