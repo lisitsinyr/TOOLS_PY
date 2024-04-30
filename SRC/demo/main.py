62e399e17af4f3d2c343e693e61b2c42eae081e4
lisitsinyr <lisitsinyr@gmail.com>
2024-04-30 15:10:58 +0400

    20240430151056

---
+++ SRC/demo/main.py
@@ -0,0 +1,14 @@
+﻿import sys
+
+
+def say_hello(name='World'):
+    return f'Hello {name}!'
+
+
+if __name__ == "__main__":
+
+    try:
+        your_name = sys.argv[1]
+        print(say_hello(your_name))
+    except IndexError:
+        print('Please enter your name.')
